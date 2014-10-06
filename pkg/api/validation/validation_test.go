d5ba4ee85d927c20eb935d268c898fd3409af2e8
Mark Turansky <markturansky@marks-mbp.attlocal.net>
2014-10-06 13:12:38 -0400

    validation of pods, services, relicationControllers, and replControllerState

--- pkg/api/validation/validation_test.go
+++ pkg/api/validation/validation_test.go
@@ -685,6 +685,7 @@ func TestValidateReplicationController(t *testing.T) {
 		}
 		for i := range errs {
 			field := errs[i].(errors.ValidationError).Field
+
 			if !strings.HasPrefix(field, "desiredState.podTemplate.") &&
 				field != "id" &&
 				field != "namespace" &&
